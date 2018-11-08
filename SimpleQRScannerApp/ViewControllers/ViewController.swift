c6b45bfbd2bff5e0bede821c7aa349d361b149c6
ljbdelacruz <ljbdelacruz123@gmail.com>
2018-11-08 17:41:05 +0800

    Initial Commit

---
+++ SimpleQRScannerApp/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SimpleQRScannerApp
+//
+//  Created by Lainel John Dela Cruz on 08/11/2018.
+//  Copyright © 2018 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
